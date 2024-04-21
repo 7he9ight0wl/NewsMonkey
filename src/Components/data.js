cf5bdd264fbb07be7396005e0c7ced3b622eb486
7he9ight0wl <mestrypradumana1999@gmail.com>
2024-04-21 17:53:48 +0530

    Your commit message

---
+++ src/Components/data.js
@@ -0,0 +1,67 @@
+export const newsdata = [
+    {
+        "source": {
+            "id": null,
+            "name": "Racingnews365.com"
+        },
+        "author": null,
+        "title": "2024 F1 Chinese Grand Prix – Qualifying results - Racingnews365.com",
+        "description": "Check out the full results from qualifying at the 2024 Chinese Grand Prix!",
+        "url": "https://racingnews365.com/2024-f1-chinese-grand-prix-qualifying-results",
+        "urlToImage": "https://cdn.racingnews365.com/2024/Verstappen/_1800x945_crop_center-center_75_none/SI202404200240_hires_jpeg_24bit_rgb.jpg?v=1713600205",
+        "publishedAt": "2024-04-20T08:14:00Z",
+        "content": "Max Verstappen secured pole position for Sunday's Chinese Grand Prix and will be joined on the front row by Sergio Perez at the Shanghai International Circuit.\r\nVerstappen was three-tenths faster tha… [+499 chars]"
+    },
+    {
+        "source": {
+            "id": null,
+            "name": "India.com"
+        },
+        "author": "https://www.india.com/author/analiza-pathak/",
+        "title": "H5N1 Bird Flu Found In Milk For First Time In 'Very High Concentrations', WHO Issues Warning - India.com",
+        "description": "The outbreak of bird flu in cow has affected at least 13 herds in six states. The reports of sick cows began to emerge from Texas, Kansas, Michigan, New Mexico, Idaho, Ohio, North Carolina and South Dakota.",
+        "url": "https://www.india.com/news/world/h5n1-bird-flu-found-in-milk-for-first-time-in-very-high-concentrations-in-us-who-issues-warning-6873072/",
+        "urlToImage": "https://static.india.com/wp-content/uploads/2024/04/cows.jpg",
+        "publishedAt": "2024-04-20T07:32:29Z",
+        "content": "The outbreak of bird flu in cow has affected at least 13 herds in six states. The reports of sick cows began to emerge from Texas, Kansas, Michigan, New Mexico, Idaho, Ohio, North Carolina and South … [+2876 chars]"
+    },
+    {
+        "source": {
+            "id": null,
+            "name": "The Indian Express"
+        },
+        "author": "The Indian Express",
+        "title": "Iran-Israel Crisis Live Updates: Air strike hits post of Iran-aligned Iraq’s Popular Mobilization Forces, one fighter killed - The Indian Express",
+        "description": null,
+        "url": "https://indianexpress.com/article/world/iran-israel-war-middle-east-crisis-gaza-news-live-updates-9278879/",
+        "urlToImage": null,
+        "publishedAt": "2024-04-20T07:25:44Z",
+        "content": null
+    },
+    {
+        "source": {
+            "id": null,
+            "name": "News18"
+        },
+        "author": "Mohammad Haris",
+        "title": "Gold Rate Falls In India: Check 22 Carat Price In Your City On April 20 - News18",
+        "description": "Gold Rate Today: Check gold rates in different cities on April 20",
+        "url": "https://www.news18.com/business/gold-rate-falls-in-india-check-22-carat-price-in-your-city-on-april-20-8859360.html",
+        "urlToImage": "https://images.news18.com/ibnlive/uploads/2023/10/untitled-design-8-2023-10-2570de316084e89c2406b18d471deb3f-16x9.jpg?impolicy=website&width=1200&height=675",
+        "publishedAt": "2024-04-20T07:15:17Z",
+        "content": "Gold Rate Today In India: After following a continuous upward trend amid Iran-Israel geopolitical tensions, gold prices in India fell on April 20. The initial price for 10 grams of gold stood at Rs 7… [+2975 chars]"
+    },
+    {
+        "source": {
+            "id": "google-news",
+            "name": "Google News"
+        },
+        "author": null,
+        "title": "On T20 World Cup Selection, Sanjay Manjrekar Warns India Selectors Against Snubbing This Star - NDTV Sports",
+        "description": "Comprehensive up-to-date news coverage, aggregated from sources all over the world by Google News.",
+        "url": "https://news.google.com/rss/articles/CBMihQFodHRwczovL3Nwb3J0cy5uZHR2LmNvbS9pcGwtMjAyNC9vbi10MjAtd29ybGQtY3VwLXNlbGVjdGlvbi1zYW5qYXktbWFuanJla2FyLXdhcm5zLWluZGlhLXNlbGVjdG9ycy1hZ2FpbnN0LXNudWJiaW5nLXRoaXMtc3Rhci01NDgyMzI50gGLAWh0dHBzOi8vc3BvcnRzLm5kdHYuY29tL2lwbC0yMDI0L29uLXQyMC13b3JsZC1jdXAtc2VsZWN0aW9uLXNhbmpheS1tYW5qcmVrYXItd2FybnMtaW5kaWEtc2VsZWN0b3JzLWFnYWluc3Qtc251YmJpbmctdGhpcy1zdGFyLTU0ODIzMjkvYW1wLzE",
+        "urlToImage": "https://lh3.googleusercontent.com/J6_coFbogxhRI9iM864NL_liGXvsQp2AupsKei7z0cNNfDvGUmWUy20nuUhkREQyrpY4bEeIBuc=s0-w300-rw",
+        "publishedAt": "2024-04-20T07:02:59Z",
+        "content": null
+    }
+]
